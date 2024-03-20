a6ba93ae0eff79ec12e44092e115afd1c7b43d77
srinivaspai07 <srinivaspai16@gmail.com>
2024-03-20 15:07:26 -0400

    Time: 30 ms (90.91%), Space: 16.4 MB (94.10%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.py
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.py
@@ -6,18 +6,17 @@ class TreeNode:
 
 class Solution:
     def leafSimilar(self, root1: TreeNode, root2: TreeNode) -> bool:
-        def get_leaf_values(root,leaf):
+        def get_leaf_values(root, leaf_list):
             if not root:
-                return 
-
+                return
             if not root.left and not root.right:
-                leaf.append(root.val)
-                return [root.val]
-            get_leaf_values(root.left,leaf)
-            get_leaf_values(root.right,leaf)
-          
-        leaf1, leaf2 = [],[]
-        get_leaf_values(root1,leaf1)
-        get_leaf_values(root2,leaf2)
+                leaf_list.append(root.val)
+                return
+            get_leaf_values(root.left, leaf_list)
+            get_leaf_values(root.right, leaf_list)
+        
+        leaf1, leaf2 = [], []
+        get_leaf_values(root1, leaf1)
+        get_leaf_values(root2, leaf2)
         
         return leaf1 == leaf2
