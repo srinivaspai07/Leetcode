2d2aa8c46759378c97bd6370becad93f4bcf4a08
srinivaspai07 <srinivaspai16@gmail.com>
2024-04-13 18:20:15 -0400

    Time: 69 ms (82.46%), Space: 18.4 MB (16.27%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -1,5 +1,26 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+
+        #working bfs sol
+
+        n = len(graph) - 1
+        result = []
+        queue = collections.deque()
+        queue.append((0, n, []))  # Initialize the queue with a tuple
+
+        while queue:
+            u, v, path = queue.popleft()
+            path.append(u)  # Append the current node to the path
+
+            if u == v:
+                result.append(path[:])  # Make a copy of the current path when the destination node is reached
+            else:
+                for neighbor in graph[u]:
+                    queue.append((neighbor, v, path[:]))  # Append neighbors with updated path
+
+        return result
+"""
+working dfs sol
         n = len(graph) - 1
         result = []
         
@@ -7,6 +28,7 @@ class Solution:
             path.append(u)
             if u == v:
                 result.append(path[:])  # Make a copy of the current path
+                #path.pop()
             else:
                 for neighbour in graph[u]:
                     dfs(neighbour, v, path)
@@ -14,3 +36,4 @@ class Solution:
         
         dfs(0, n, [])
         return result
+"""
