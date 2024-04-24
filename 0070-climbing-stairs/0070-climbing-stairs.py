73a973bb51d749458804a5517cd5027fc5063548
srinivaspai07 <srinivaspai16@gmail.com>
2024-04-24 13:13:11 -0400

    Time: 33 ms (67.27%), Space: 16.6 MB (30.87%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -15,4 +15,20 @@ class Solution:
         
         return dp[n-1]
         
+"""
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        def dp(i):
+            if i <= 2: 
+                return i
+            if i not in memo:
+                # Instead of just returning dp(i - 1) + dp(i - 2), calculate it once and then
+                # store the result inside a hashmap to refer to in the future.
+                memo[i] = dp(i - 1) + dp(i - 2)
+            
+            return memo[i]
         
+        memo = {}
+        return dp(n)
+"""
