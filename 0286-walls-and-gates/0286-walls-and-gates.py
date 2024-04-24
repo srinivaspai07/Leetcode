5bbc9f6048a1cc83c2e7a4d8bea6dcd4ff519620
srinivaspai07 <srinivaspai16@gmail.com>
2024-04-23 20:11:44 -0400

    Time: 190 ms (54.97%), Space: 19.6 MB (54.79%) - LeetHub

---
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+class Solution:
+    def wallsAndGates(self, rooms: List[List[int]]) -> None:
+        if not rooms:
+            return
+
+        rows = len(rooms)
+        cols = len(rooms[0])
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+        # Find gates and initialize the queue with them
+        queue = deque()
+        for i in range(rows):
+            for j in range(cols):
+                if rooms[i][j] == 0:
+                    queue.append((i, j))
+
+        while queue:
+            row, col = queue.popleft()
+            distance = rooms[row][col] + 1  # Distance to be filled in adjacent cells
+            print(distance)
+
+            for dr, dc in directions:
+                new_row, new_col = row + dr, col + dc
+                if 0 <= new_row < rows and 0 <= new_col < cols and rooms[new_row][new_col] == 2147483647:
+                    rooms[new_row][new_col] = distance
+                    queue.append((new_row, new_col))
+
+        return
