bac9cd41d30d6c855aa24138a47340d84d5573a3
srinivaspai07 <srinivaspai16@gmail.com>
2024-04-25 18:41:54 -0400

    Time: 78 ms (23.87%), Space: 17 MB (86.30%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,19 +1,34 @@
+from typing import List
+
 class Solution:
+    """
+    A class to solve the maxProduct problem.
+
+    Attributes:
+    None
+    """
+    
     def maxProduct(self, nums: List[int]) -> int:
-        if len(nums) == 0:
+        """
+        Solves the maxProduct problem.
+
+        Args:
+        nums: A list of integers.
+
+        Returns:
+        The maximum product of any contiguous subarray.
+        """
+        if not nums:
             return 0
 
         max_so_far = nums[0]
         min_so_far = nums[0]
         result = max_so_far
 
-        for i in range(1, len(nums)):
-            curr = nums[i]
-            temp_max = max(curr, max_so_far * curr, min_so_far * curr)
-            min_so_far = min(curr, max_so_far * curr, min_so_far * curr)
-
+        for num in nums[1:]:
+            temp_max = max(num, max_so_far * num, min_so_far * num)
+            min_so_far = min(num, max_so_far * num, min_so_far * num)
             max_so_far = temp_max
-
             result = max(max_so_far, result)
 
         return result
