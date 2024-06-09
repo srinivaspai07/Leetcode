5278ddb367caefa4144494856d6101306378e557
srinivaspai07 <srinivaspai16@gmail.com>
2024-06-09 01:35:13 -0400

    Time: 50 ms (5.09%), Space: 16.5 MB (93.35%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -8,10 +8,8 @@ class Solution:
             for num in nums:
                 if num not in curr:
                     curr.append(num)
-                    print(f"num is {num} and cur is {curr}")
                     backtrack(curr)
                     curr.pop()
-                    print(f"after is num is {num} and cur is {curr}")
 
         ans = []
         backtrack([])
