3c9403805e7f2d200c78f2c6df47d3e77586781a
srinivaspai07 <srinivaspai16@gmail.com>
2024-06-09 00:13:58 -0400

    Time: 40 ms (34.28%), Space: 16.7 MB (74.16%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -20,3 +20,33 @@ class Solution:
         for k in range(n + 1):
             backtrack()
         return output
+"""
+NEETCODE SOLUTION
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+
+        res = []
+        cur = []
+        def backtrack(i):
+
+            if i >=len(nums):
+                print(f"inside and i is {i} and cur is {cur}")
+                res.append(cur.copy())
+                return
+            
+            cur.append(nums[i])
+            print(f"first backtrack is {i} and cur is {cur}")
+
+            backtrack(i+1)
+
+            cur.pop()
+            print(f"just before second backtrack is {i} and cur is {cur}")      
+            backtrack(i+1)
+            print(f"after backtrack is {i} and cur is {cur}")
+
+        backtrack(0)
+        return res
+
+"""
+        
+        
