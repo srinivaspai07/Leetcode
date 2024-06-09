9dbe205ab63d3e62c252ba057d1a87d4be937a74
srinivaspai07 <srinivaspai16@gmail.com>
2024-06-09 00:13:24 -0400

    Time: 32 ms (85.07%), Space: 16.7 MB (74.16%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,28 +1,22 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-
-        res = []
-        cur = []
-        def backtrack(i):
-
-            if i >=len(nums):
-                print(f"inside and i is {i} and cur is {cur}")
-                res.append(cur.copy())
+        def backtrack(first=0, curr=[]):
+            # If the combination is done
+            if len(curr) == k:
+                output.append(curr[:])
                 return
-            
-            cur.append(nums[i])
-            print(f"first backtrack is {i} and cur is {cur}")
+            for i in range(first, n):
+                # Add nums[i] into the current combination
+                curr.append(nums[i])
 
-            backtrack(i+1)
+                # Use the next integers to complete the combination
+                backtrack(i + 1, curr)
 
-            cur.pop()
-            print(f"just before second backtrack is {i} and cur is {cur}")      
-            backtrack(i+1)
-            print(f"after backtrack is {i} and cur is {cur}")
+                # Backtrack
+                curr.pop()
 
-        backtrack(0)
-        return res
-
-
-        
-        
+        output = []
+        n = len(nums)
+        for k in range(n + 1):
+            backtrack()
+        return output
