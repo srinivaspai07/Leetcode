be55d72a80933cca9370f3312338937a6d031673
srinivaspai07 <srinivaspai16@gmail.com>
2024-10-10 12:10:52 -0700

    Time: 179 ms (81.50%), Space: 17.7 MB (35.00%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,52 +1,21 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:        
         n = len(isConnected)
-        visited = [False] * n  # Track whether each city has been visited
+        visited = set()  # Track whether each city has been visited
         provinces = 0
 
         # DFS function to visit all cities in the same province
         def dfs(city):
             for neighbor in range(n):
-                if isConnected[city][neighbor] == 1 and not visited[neighbor]:
-                    visited[neighbor] = True  # Mark as visited
+                if isConnected[city][neighbor] == 1 and neighbor not in visited:
+                    visited.add(neighbor)  # Mark as visited
                     dfs(neighbor)  # Recursively visit all its neighbors
 
         # Loop over all cities and run DFS for unvisited cities
         for city in range(n):
-            if not visited[city]:
-                visited[city] = True  # Mark city as visited
+            if city not in visited:
+                visited.add(city)  # Mark city as visited
                 dfs(city)  # Visit all cities in this province
                 provinces += 1  # Count this as one province
 
         return provinces
-
-    
-# this is a good problem
-# this is not a grid traversal problem
-# here, the graph is given as adjacency matrix and this problem teaches us how to traverse adjancy matrix
-
-# for added info, if the input was given as adjacency list instead of matrix, below would be the solution
-
-def findCircleNum(adj_list):
-    # Number of cities
-    n = len(adj_list)
-    
-    # To track visited cities
-    visited = set()
-    provinces = 0
-
-    # DFS to mark all connected cities
-    def dfs(city):
-        for neighbor in adj_list[city]:
-            if neighbor not in visited:
-                visited.add(neighbor)
-                dfs(neighbor)
-
-    # Visit each city and run DFS if not visited
-    for city in range(n):
-        if city not in visited:
-            visited.add(city)  # Mark city as visited
-            dfs(city)          # Visit all cities in this province
-            provinces += 1     # Increment province count
-
-    return provinces
