afb54037157916284046b73aa245c5117e56f417
srinivaspai07 <srinivaspai16@gmail.com>
2024-11-22 15:51:51 -0800

    Time: 3 ms (71.11%), Space: 16.7 MB (68.42%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -3,9 +3,32 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+
+        carry = 0
+        dummy = ListNode(0)
+        newList = ListNode(0)
+        dummy = newList
         
+        while l1:
+            
+            num = l1.val + l2.val
+            carry = num//10
+            num = num % 10
+            
+            node = ListNode(num)
+            newList.next = node
+            newList = node
+        
+        return dummy.next.next
+            
+            
+            
+            
+        
+"""        
         fp = l1
         sp = l2
         fl = []
@@ -49,4 +72,22 @@ class Solution:
             node = node.next
         
         return(dummypointer.next)
-
+"""
+class Solution:
+    def addTwoNumbers(
+        self, l1: Optional[ListNode], l2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        dummyHead = ListNode(0)
+        curr = dummyHead
+        carry = 0
+        while l1 != None or l2 != None or carry != 0:
+            l1Val = l1.val if l1 else 0
+            l2Val = l2.val if l2 else 0
+            columnSum = l1Val + l2Val + carry
+            carry = columnSum // 10
+            newNode = ListNode(columnSum % 10)
+            curr.next = newNode
+            curr = newNode
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        return dummyHead.next
