9cc745e437a18397282f493ae9cf9f76077359fc
srinivaspai07 <srinivaspai16@gmail.com>
2024-11-24 18:13:55 -0800

    Time: 23 ms (53.17%), Space: 29.9 MB (34.01%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,8 +1,8 @@
-
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
         num_indices = {}  # Dictionary to store indices of numbers
-
+        
+        # Iterate through the array
         for i, num in enumerate(nums):
             # If the number is already in the dictionary and the absolute difference
             # between the current index and the previous index is less than or equal to k,
@@ -14,3 +14,22 @@ class Solution:
 
         # If no such pair of indices is found, return False
         return False
+
+        # Removed misplaced code block with incorrect indentation
+        """
+          does not work for all inputs... example:
+ input [1,2,3,1,2,3] , k = 2
+Output
+true
+Expected
+false
+
+        nums.sort()
+        
+        for i in range(len(nums)-1):
+            
+            if (abs(nums[i] - nums[i]-1)) <= k:
+                return True
+        return False
+        """
+  
